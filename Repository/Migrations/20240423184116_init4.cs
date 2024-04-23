8ccdc7b8b0575cf0e79dc57a35c4c3a66f51e23a
neslihancaparoglu <neslihancaparoglu@gmail.com>
2024-04-23 21:54:29 +0300

    roomtype kaydetme hatası giderildi

---
+++ Repository/Migrations/20240423184116_init4.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class init4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AboutUs");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoomType",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomType",
+                table: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "AboutUs",
+                columns: table => new
+                {
+                    AboutUsId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AboutUsDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AboutUsImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AboutUsTitle = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AboutUs", x => x.AboutUsId);
+                });
+        }
+    }
+}
